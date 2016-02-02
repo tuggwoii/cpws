e571c498ef1d85d2db72c989601128f87c26eb45
Tak <tak@clearsumm.it>
2016-02-02 17:50:10 +0700

    block user to access dashboard if not login, update angular version,add profile screen

---
+++ src/static/js/modules/dashboard/controllers/dashboard-controller.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+module.controller('DashboardController', ['$scope', '$cookies', 'AccountService', 'NotificationService', function ($scope, $cookies, AccountService, NotificationService) {
+    $scope.state = {};
+    $scope.user = {};
+
+    $scope.onLoad = function () {
+        $scope.state.isReady = false;
+        $scope.requestUser();
+    };
+
+    $scope.requestUser = function () {
+        NotificationService.loading();
+        AccountService.me()
+            .success(function (response) {
+                $scope.user = response.data;
+                NotificationService.stopLoading();
+            }).error(function () {
+                window.location.href = '/login';
+            });
+    };
+
+
+    $scope.$on('Ready', function () {
+        $scope.state.isReady = true;
+    });
+
+    $scope.onLoad();
+
+}]);
